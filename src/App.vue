5c712e74c387ca0a7e92a0594f94eb58d5d0b540
Kuludu <i@kuludu.net>
2022-01-04 10:51:42 +0800

    Init HeadBar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeadBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeadBar from "@/components/HeadBar";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeadBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/HeadBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-navbar type="light" variant="info" class="bg-light">
+    <b-navbar-brand variant="faded" class="container-fluid">RIPS</b-navbar-brand>
+    <b-navbar-nav class="ms-auto">
+      <b-nav-item>大区流量</b-nav-item>
+      <b-nav-item>站点流量</b-nav-item>
+      <b-nav-item>轨交特情</b-nav-item>
+      <b-nav-item>管理登陆</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "HeadBar"
+}
+</script>
+
+<style scoped>
+
+</style>
