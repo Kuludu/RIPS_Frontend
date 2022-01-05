3e8e9922a9afd55709bbc5fcb3bce14db1e081ec
Kuludu <i@kuludu.net>
2022-01-05 17:39:03 +0800

    Edit HeadBar

--- src/components/HeadBar.vue
+++ src/components/HeadBar.vue
@@ -2,10 +2,10 @@
   <b-navbar type="light" variant="info" class="bg-light">
     <b-navbar-brand variant="faded" class="container-fluid">轨道交通智能预测系统(RIPS)</b-navbar-brand>
     <b-navbar-nav class="ms-auto">
-      <b-nav-item><b-button><router-link to="/line">线路流量</router-link></b-button></b-nav-item>
-      <b-nav-item><b-button><router-link to="/site">站点流量</router-link></b-button></b-nav-item>
-      <b-nav-item><b-button><router-link to="/situation">轨交特情</router-link></b-button></b-nav-item>
-      <b-nav-item><b-button><router-link to="/admin/login">管理登陆</router-link></b-button></b-nav-item>
+      <b-nav-item><b-button variant="outline-primary"><router-link to="/line">线路流量</router-link></b-button></b-nav-item>
+      <b-nav-item><b-button variant="outline-primary"><router-link to="/site">站点流量</router-link></b-button></b-nav-item>
+      <b-nav-item><b-button variant="outline-primary"><router-link to="/situation">轨交特情</router-link></b-button></b-nav-item>
+      <b-nav-item><b-button variant="outline-primary"><router-link to="/admin/login">管理登陆</router-link></b-button></b-nav-item>
     </b-navbar-nav>
   </b-navbar>
 </template>
@@ -17,5 +17,8 @@ export default {
 </script>
 
 <style scoped>
-
+a, a:hover {
+  text-decoration: none;
+  color: black;
+}
 </style>
