368357fc23359ac13ac9534cfa8018fba6ecb72c
Kuludu <i@kuludu.net>
2022-01-05 18:05:35 +0800

    Edit Default

--- src/components/Default.vue
+++ src/components/Default.vue
@@ -1,7 +1,8 @@
 <template>
-  <b-container class="text-center" style="margin-top: 20px">
+  <b-container class="text-center" style="margin-top: 120px">
     <h1>轨道交通智能预测系统</h1>
     <h2>Railway Intelligent Predicting System</h2>
+    <b-img :src="require('@/assets/pic.png')"/>
   </b-container>
 </template>
 
