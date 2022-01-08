41bb80ff7988f934f437f840c7bb02e6b77bb82a
Kuludu <i@kuludu.net>
2022-01-08 21:56:14 +0800

    Edit FootBar

--- src/components/FootBar.vue
+++ src/components/FootBar.vue
@@ -2,7 +2,7 @@
   <div>
     <footer class="bg-light">
       <div class="p-3 footer" style="background-color: rgba(0, 0, 0, 0.2);">
-        加载时间：{{ getTime() }}
+        加载时间（本地）：{{ getTime() }}
       </div>
     </footer>
   </div>
