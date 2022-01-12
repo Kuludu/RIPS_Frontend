2f6f13f391b4c524e6a893eac4e9225fa5ab8098
Kuludu <i@kuludu.net>
2022-01-12 14:43:10 +0800

    Edit Default

--- src/components/Default.vue
+++ src/components/Default.vue
@@ -3,6 +3,7 @@
     <h1>轨道交通智能预测系统</h1>
     <h2>Railway Intelligent Predicting System</h2>
     <b-img :src="require('@/assets/pic.png')"/>
+    <h5>建议在宽屏上使用Chrome浏览器浏览以保证最佳效果。</h5>
   </b-container>
 </template>
 
