8b99851dd49dc2c341ee80e9923039b63c426d54
Kuludu <i@kuludu.net>
2022-01-16 14:10:19 +0800

    Edit Situation

--- src/components/Situation.vue
+++ src/components/Situation.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <h1>当前特情：</h1>
+    <b-container class="text-center">
+      <h1>当前轨道交通系统特殊情况</h1>
+      <b-table striped fixed :items="items" :fields="fields" />
+    </b-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Situation"
+  name: "Situation",
+  data: function () {
+    return {
+      fields: ['特情名称', '发布时间'],
+      items: []
+    }
+  },
+  mounted() {
+    this.axios({
+      method: "get",
+      url: "/api/situation"
+    }).then(resp => {
+      this.items = resp.data["0"]
+    }).catch(e => {
+      console.log(e)
+      alert("后端服务器错误")
+    })
+  }
 }
 </script>
 
