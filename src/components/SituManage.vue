6a531948166477eeeaadb8af5a1bdad4affd32e8
Kuludu <i@kuludu.net>
2022-01-21 17:39:18 +0800

    Edit SituManage

--- src/components/SituManage.vue
+++ src/components/SituManage.vue
@@ -1,67 +1,61 @@
 <template>
   <div>
-    <b-sidebar class="text-center" visible no-header-close>
-      <router-link to="/admin/parameter"><b-button pill variant="primary">参数设置</b-button></router-link>
-      <router-link to="/admin/situation"><b-button pill variant="primary">特情管理</b-button></router-link>
+    <b-sidebar class="text-center" no-header-close visible>
+      <router-link to="/admin/parameter">
+        <b-button pill variant="primary">参数设置</b-button>
+      </router-link>
+      <router-link to="/admin/situation">
+        <b-button pill variant="primary">特情管理</b-button>
+      </router-link>
     </b-sidebar>
     <div class="container col-md-6" style="margin-top: 20px">
       <h1>特情管理</h1>
-      <b-table
-          :items="items"
-          :fields="fields"
-          select-mode="multi"
-          responsive="sm"
-          ref="selectableTable"
-          selectable
-          @row-selected="onRowSelected"
-      >
-        <template #cell(selected)="{ rowSelected }">
-          <template v-if="rowSelected">
-            <span aria-hidden="true">&check;</span>
-            <span class="sr-only">Selected</span>
+      <b-card title="特情面板">
+        <b-table
+            ref="selectableTable"
+            :fields="fields"
+            :items="items"
+            responsive="sm"
+            select-mode="multi"
+            selectable
+            @row-selected="onRowSelected"
+        >
+          <template #cell(selected)="{ rowSelected }">
+            <template v-if="rowSelected">
+              <span aria-hidden="true">&check;</span>
+              <span class="sr-only">Selected</span>
+            </template>
+            <template v-else>
+              <span aria-hidden="true">&nbsp;</span>
+              <span class="sr-only">Not selected</span>
+            </template>
           </template>
-          <template v-else>
-            <span aria-hidden="true">&nbsp;</span>
-            <span class="sr-only">Not selected</span>
-          </template>
-        </template>
-        <template #table-caption>当前全部特情</template>
-      </b-table>
-      <p>
-        <b-button-group>
-          <b-button variant="primary">删除</b-button>
-          <b-button variant="primary">激活</b-button>
-          <b-button variant="primary">隐藏</b-button>
-        </b-button-group>
-      </p>
+          <template #table-caption>当前全部特情</template>
+        </b-table>
+        <p>
+          <b-button-group>
+            <b-button variant="primary" @click="deleteSituation">删除</b-button>
+            <b-button variant="primary" @click="activateSituation">激活</b-button>
+            <b-button variant="primary" @click="deactivateSituation">隐藏</b-button>
+          </b-button-group>
+        </p>
+      </b-card>
 
       <b-card title="发布特情">
-        <b-form @submit="submit">
-          <b-form-group
-              label="特情选择:"
-          >
-            <b-form-select v-model="form.situation" label="轨交特情">
-              <b-form-select-option value="0">请选择特情</b-form-select-option>
-              <b-form-select-option value="1">雨雪特情</b-form-select-option>
-              <b-form-select-option value="2" disabled>暴力恐怖事件</b-form-select-option>
-              <b-form-select-option value="3" disabled>设备故障</b-form-select-option>
-            </b-form-select>
-          </b-form-group>
-          <b-form-group
-              label="特情名称:"
-              class="col-3"
-          >
-            <b-form-input placeholder="请输入特情名称"></b-form-input>
-          </b-form-group>
-          <b-form-group
-              label="发布时间:"
-              class="col-3"
-          >
-            <b-form-datepicker locale="zh" />
-            <b-form-timepicker locale="zh" />
-          </b-form-group>
-          <b-button type="submit" variant="primary" style="margin-top: 10px">提交</b-button>
-        </b-form>
+        <b-form-group
+            class="col-3"
+            label="特情名称:"
+        >
+          <b-form-input v-model="name" placeholder="请输入特情名称"></b-form-input>
+        </b-form-group>
+        <b-form-group
+            class="col-3"
+            label="发布时间:"
+        >
+          <b-form-datepicker v-model="date" locale="zh"/>
+          <b-form-timepicker v-model="time" locale="zh"/>
+        </b-form-group>
+        <b-button style="margin-top: 10px" variant="primary" @click="createSituation">发布</b-button>
       </b-card>
     </div>
   </div>
@@ -72,28 +66,128 @@ import qs from "qs";
 
 export default {
   name: "SituManage",
-  data: function() {
+  data: function () {
     return {
-      form: {
-        situation: "",
-        traffic: ""
-      },
+      name: "",
+      date: "",
+      time: "",
       fields: ['ID', '是否激活', '特情名称', '发布时间'],
       items: [],
       selected: []
     }
   },
+  mounted() {
+    this.fetchAll();
+  },
   methods: {
+    fetchAll() {
+      this.axios({
+        method: "get",
+        url: "/api/situation/fetchAll"
+      }).then(resp => {
+        this.items = resp.data["situation_all"]
+      }).catch(e => {
+        console.log(e)
+        alert("后端服务器错误")
+      })
+    },
     onRowSelected(items) {
       this.selected = items
     },
-    submit: function(event) {
-      event.preventDefault()
+    deleteSituation() {
+      if (this.selected.length > 0) {
+        let onDelete = []
+        this.selected.forEach(item => {
+          onDelete.push(item["ID"])
+        })
+        this.axios({
+          method: "post",
+          url: "/api/situation/delete",
+          data: qs.stringify({
+            situation_id: onDelete
+          }, {
+            arrayFormat: 'repeat'
+          })
+        }).then(resp => {
+          if (resp.data === "success") {
+            alert("删除成功!")
+          } else {
+            alert("删除失败!")
+          }
+          this.fetchAll()
+        }).catch(e => {
+          console.log(e)
+          alert("后端服务器错误")
+        })
+      } else {
+        alert("请选择要删除的特情")
+      }
+    },
+    activateSituation() {
+      if (this.selected.length > 0) {
+        let onActivate = []
+        this.selected.forEach(item => {
+          onActivate.push(item["ID"])
+        })
+        this.axios({
+          method: "post",
+          url: "/api/situation/activate",
+          data: qs.stringify({
+            situation_id: onActivate
+          }, {
+            arrayFormat: 'repeat'
+          })
+        }).then(resp => {
+          if (resp.data === "success") {
+            alert("激活成功!")
+          } else {
+            alert("激活失败!")
+          }
+          this.fetchAll()
+        }).catch(e => {
+          console.log(e)
+          alert("后端服务器错误")
+        })
+      } else {
+        alert("请选择要激活的特情")
+      }
+    },
+    deactivateSituation() {
+      if (this.selected.length > 0) {
+        let onDeactivate = []
+        this.selected.forEach(item => {
+          onDeactivate.push(item["ID"])
+        })
+        this.axios({
+          method: "post",
+          url: "/api/situation/deactivate",
+          data: qs.stringify({
+            situation_id: onDeactivate
+          }, {
+            arrayFormat: 'repeat'
+          })
+        }).then(resp => {
+          if (resp.data === "success") {
+            alert("隐藏成功!")
+          } else {
+            alert("隐藏失败!")
+          }
+          this.fetchAll()
+        }).catch(e => {
+          console.log(e)
+          alert("后端服务器错误")
+        })
+      } else {
+        alert("请选择要隐藏的特情")
+      }
+    },
+    createSituation() {
       this.axios({
         method: "post",
-        url: "/api/parameter",
+        url: "/api/situation/create",
         data: qs.stringify({
-          config: this.form.config,
+          name: this.name,
+          datetime: this.date + " " + this.time
         })
       }).then(resp => {
         if (resp.data !== "success")
