15ba9d2cac05604284eb6e389c0d63b64cdc6b25
Kuludu <i@kuludu.net>
2022-01-21 22:10:01 +0800

    Edit Parameter

--- src/components/Parameter.vue
+++ src/components/Parameter.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-    <b-sidebar class="text-center" visible no-header-close>
-      <router-link to="/admin/parameter"><b-button pill variant="primary">参数设置</b-button></router-link>
-      <router-link to="/admin/situation"><b-button pill variant="primary">特情管理</b-button></router-link>
+    <b-sidebar class="text-center" no-header-close visible>
+      <router-link to="/admin/parameter">
+        <b-button pill variant="primary">参数设置</b-button>
+      </router-link>
+      <router-link to="/admin/situation">
+        <b-button pill variant="primary">特情管理</b-button>
+      </router-link>
     </b-sidebar>
-    <b-container class="col-3" style="margin-top: 20px">
+    <b-container class="col-6 mt-3">
       <h1>系统参数设置</h1>
       <small>此参数应由专业人员配置，站点工作人员请勿随意更改！</small>
-      <form @submit="submit">
-        <b-textarea v-model="form.config" style="height: 200px" placeholder="如果您不清楚如何配置，请勿修改！"/>
-        <b-button type="submit" variant="primary" style="margin-top: 10px">提交</b-button>
-      </form>
+      <b-card title="站点压力阈值">
+        <b-row>
+          <b-col>
+            <b-form-select v-model="line_selected" :options="line_options"/>
+          </b-col>
+          <b-col>
+            <b-form-select v-model="site_selected" :options="site_options[this.line_selected]" @change="refresh_site"/>
+          </b-col>
+        </b-row>
+        <p class="mt-3">当前站点压力阈值：{{ this.site_pressure_threshold }}</p>
+        <b-form-input class="col-3" placeholder="请输入新设置的压力阈值" type="number" v-model="new_threshold"></b-form-input>
+        <b-button class="mt-3" @click="submit_threshold">提交</b-button>
+      </b-card>
     </b-container>
   </div>
 </template>
@@ -22,29 +35,72 @@ export default {
   name: "Parameter",
   data: function () {
     return {
-      form: {
-        config: ""
-      }
+      line_selected: null,
+      site_selected: null,
+      new_threshold: null,
+      line_options: [
+        {value: null, text: '请选择线路'},
+        {value: '1', text: '1号线', disabled: true},
+        {value: '2', text: '2号线', disabled: true},
+        {value: '3', text: '3号线', disabled: true},
+        {value: '4', text: '4号线', disabled: true},
+        {value: '5', text: '5号线'},
+        {value: '6', text: '6号线', disabled: true},
+      ],
+      site_options: {
+        null: [{
+          value: null,
+          text: '请先选择线路'
+        }],
+        '5': [
+          {value: null, text: '请选择站点'},
+          {value: '1', text: '站点1'},
+          {value: '2', text: '站点2'},
+          {value: '3', text: '站点3'},
+          {value: '4', text: '站点4'},
+          {value: '5', text: '站点5'},
+          {value: '6', text: '站点6'},
+          {value: '7', text: '站点7'},
+          {value: '8', text: '站点8'}
+        ]
+      },
+      site_pressure_threshold: 0
     }
   },
   methods: {
-    submit: function (event) {
-      event.preventDefault()
+    refresh_site() {
       this.axios({
         method: "post",
-        url: "/api/parameter",
+        url: "/api/parameter/getThreshold",
         data: qs.stringify({
-          config: this.form.config,
+          line: this.line_selected,
+          site: this.site_selected,
+        })
+      }).then(resp => {
+        this.site_pressure_threshold = resp.data['threshold'];
+      }).catch(e => {
+        console.log(e)
+        alert("后端服务器错误")
+      })
+    },
+    submit_threshold() {
+      this.axios({
+        method: "post",
+        url: "/api/parameter/setThreshold",
+        data: qs.stringify({
+          line: this.line_selected,
+          site: this.site_selected,
+          threshold: this.new_threshold
         })
       }).then(resp => {
         if (resp.data !== "success")
           alert("配置保存错误！")
         else
           alert("配置保存成功！")
+        this.refresh_site()
       }).catch(e => {
         console.log(e)
         alert("后端服务器错误")
-        this.$router.push({path: '/admin/login'})
       })
     }
   }
