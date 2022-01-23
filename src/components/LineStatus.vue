7ce93d6b311a0409b6683fc0ae3d22e1a813b828
Kuludu <i@kuludu.net>
2022-01-23 19:23:04 +0800

    Edit LineStatus

--- src/components/LineStatus.vue
+++ src/components/LineStatus.vue
@@ -46,28 +46,7 @@ export default {
     }
   },
   mounted: function () {
-    let line = this.echarts.init(document.getElementById('line'));
-    const parser = new DOMParser();
-    const line_5 =
-        "<svg width=\"1370\" height=\"900\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
-        " <title>毕业设计-车站线路</title>\n" +
-        " <g id=\"Layer_1\">\n" +
-        "  <title>line_5</title>\n" +
-        "  <line stroke=\"#bfbf00\" stroke-dasharray=\"5,5\" fill=\"none\" stroke-width=\"10\" x1=\"463.98186\" y1=\"88.40089\" x2=\"463.98186\" y2=\"722.35158\" id=\"svg_1\"/>\n" +
-        "  <line stroke=\"#bfbf00\" stroke-dasharray=\"5,5\" fill=\"none\" stroke-width=\"10\" x1=\"149.37503\" y1=\"88.40089\" x2=\"149.37503\" y2=\"722.35158\" id=\"svg_2\"/>\n" +
-        "  <line stroke=\"#bfbf00\" stroke-dasharray=\"5,5\" fill=\"none\"  stroke-width=\"10\" x1=\"150.4167\" y1=\"718.75001\" x2=\"467.1262\" y2=\"718.75001\" id=\"svg_3\"/>\n" +
-        "  <circle name=\"站点1\" stroke=\"null\" r=\"30\" cy=\"89.95012\" cx=\"149.99651\" id=\"svg_4\"/>\n" +
-        "  <circle name=\"站点2\" stroke=\"null\" r=\"30\" cy=\"290.76027\" cx=\"148.99651\" id=\"svg_5\"/>\n" +
-        "  <circle name=\"站点3\" stroke=\"null\" r=\"30\" cy=\"507.57166\" cx=\"148.99651\" id=\"svg_6\"/>\n" +
-        "  <circle name=\"站点4\" stroke=\"null\" r=\"30\" cy=\"718.81752\" cx=\"148.99651\" id=\"svg_7\"/>\n" +
-        "  <circle name=\"站点5\" stroke=\"null\" r=\"30\" cy=\"718.76146\" cx=\"465.03246\" id=\"svg_8\"/>\n" +
-        "  <circle name=\"站点6\" stroke=\"null\" r=\"30\" cy=\"509.69681\" cx=\"464.14045\" id=\"svg_9\"/>\n" +
-        "  <circle name=\"站点7\" stroke=\"null\" r=\"30\" cy=\"291.14973\" cx=\"464.52746\" id=\"svg_10\"/>\n" +
-        "  <circle name=\"站点8\" stroke=\"null\" r=\"30\" cy=\"92.23925\" cx=\"464.52746\" id=\"svg_11\"/>\n" +
-        " </g>\n" +
-        "</svg>";
-
-    this.echarts.registerMap('line_5', {svg: parser.parseFromString(line_5, 'text/xml')});
+    let line = this.echarts.init(document.getElementById('line'))
     this.axios({
       method: "get",
       url: "/api/line/" + this.$route.params.id
@@ -84,7 +63,7 @@ export default {
           trigger: 'item',
         },
         title: {
-          text: '5号线',
+          text: this.$route.params.id + '号线',
           subtext: '线路运行状态',
           y: 'top',
           left: 'center',
@@ -118,9 +97,9 @@ export default {
         },
         series: [
           {
-            name: '5号线站点',
+            name: '站点',
             type: 'map',
-            map: 'line_5',
+            map: 'line_' + this.$route.params.id,
             center: [830, 400],
             selectedMode: false,
             emphasis: {
